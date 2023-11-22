a1d2bb716eb5174fd94bf34077e663f0bc0609cc
nureddyn <nureddynballout@gmail.com>
2023-11-22 16:58:46 -0300

    Practice done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
+  const [form, setForm] = useState([])
+
+  const studentName = useRef(null)
+  const grade = useRef(null)
+  const assignment = useRef(null)
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    
+    setForm([...form, {
+      'name': studentName.current.value,
+      'assignment': assignment.current.value,
+      'grade': grade.current.value}])
+
+    studentName.current.value = ''
+    grade.current.value = ''
+    assignment.current.value = ''
+  }
+// ['one', 'two']; 0, 1
+  const displayList = () => {
+    return form.map((row, index) => (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.assignment}</td>
+        <td>{row.grade}</td>
+      </tr>
+    ));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>Gradebook</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" ref={studentName} placeholder='Student name' />
+          <input type="text" ref={assignment} placeholder='Assignment' />
+          <input type="number" ref={grade} placeholder='Student grade'/>
+          <input type="submit" value='Submit Form' />
+        </form>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Student</th>
+            <th>Assignment</th>
+            <th>Grade</th>
+          </tr>
+        </thead>
+        <tbody>
+          {displayList()}
+        </tbody>
+      </table>
+    </>
   );
 }
 
